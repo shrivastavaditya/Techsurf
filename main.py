f36a7896d0edc71f8494b6d8d0dcd05ca4d4a1b8
shrivastavaditya <113035198+shrivastavaditya@users.noreply.github.com>
2023-08-17 15:09:49 +0530

    new chat page added

--- main.py
+++ main.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template("home.html")
+
+@app.route('/chat.html')
+def chat():
+    return render_template("chat.html")
 
 app.run(debug=True)
