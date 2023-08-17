05dc608f7a0582a02eee9c7d09fbf7d872a0fc77
shrivastavaditya <113035198+shrivastavaditya@users.noreply.github.com>
2023-08-17 23:58:47 +0530

    all pages initialisation

--- main.py
+++ main.py
@@ -9,4 +9,36 @@ def home():
 def chat():
     return render_template("chat.html")
 
+@app.route('/technical.html')
+def tech():
+    return render_template("technical.html")
+
+@app.route('/academic.html')
+def aca():
+    return render_template("academic.html")
+
+@app.route('/blog.html')
+def blog():
+    return render_template("blog.html")
+
+@app.route('/ecom.html')
+def eco():
+    return render_template("ecom.html")
+
+@app.route('/market.html')
+def mkt():
+    return render_template("market.html")
+
+@app.route('/news.html')
+def news():
+    return render_template("news.html")
+
+@app.route('/social.html')
+def soc():
+    return render_template("social.html")
+
+@app.route('/translate.html')
+def translation():
+    return render_template("translate.html")
+
 app.run(debug=True)
