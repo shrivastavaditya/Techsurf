209b7d8c418edba57171f62bd873acc8a6660ac2
shrivastavaditya <113035198+shrivastavaditya@users.noreply.github.com>
2023-08-20 05:52:56 +0530

    error in fetching answer from open ai

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+from flask_pymongo import PyMongo
+import openai
+
+openai.api_key = "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"
+
+
+
+
+
 app = Flask(__name__)
+app.config["MONGO_URI"] = "mongodb+srv://adityaryk:aditya@cluster0.dqf0sje.mongodb.net/news"
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def home():
@@ -31,6 +43,9 @@ def mkt():
 
 @app.route('/news.html')
 def news():
+    qstn = mongo.db.qstn.find({})
+    myQstn = [qst for qst in qstn]
+    print(myQstn)
     return render_template("news.html")
 
 @app.route('/social.html')
@@ -40,5 +55,31 @@ def soc():
 @app.route('/translate.html')
 def translation():
     return render_template("translate.html")
+
+@app.route('/api',methods=["GET","POST"])
+def qa():
+    if request.method =="POST":
+        print(request.json)
+        qstn = request.json.get("question")
+        qst = mongo.db.qstn.find_one({"question": qstn})
+        print(qst)
+        if qst:
+            data={"result": f"{qst['answer']}"}
+            return jsonify(data)
+        else:
+            response = openai.Completion.create(
+               model="text-davinci-003",
+               prompt=qstn,
+               temperature=0.7,
+               max_tokens=256,
+               top_p=1,
+               frequency_penalty=0,
+               presence_penalty=0
+            )
+            data={"question": qstn, "answer": response["choices"][0]["text"]}
+            mongo.db.qstn.insert_one( {"question": qstn, "answer": response["choices"][0]["text"]} )
+            return jsonify(data)
+    data={"result":"Aditya hi data hai"}
+    return jsonify(data)
 
 app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,28 @@
+//Post Method Implementation
+async function postData(url = "", data = {}) {
+    const response = await fetch(url, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+    return response.json();
+}
+
+writeButton.addEventListener("click", async() => {
+    des = document.getElementById("description").value;
+    document.getElementById("description").value = "";
+    lang = document.getElementById("language").value;
+    document.getElementById("language").value = "";
+    ton = document.getElementById("tone").value;
+    document.getElementById("tone").value = "";
+
+    //Full question for openAi
+    question = "Please write a news article on " + des + " in " + lang + " language in " + ton + " tone with proper indentation.";
+    //alert(question)
+
+    //Answer to be shown in the textarea ansbox
+    let result = await postData('/api', { "question": question })
+    ansbox.innerHTML = result.answer
+})
